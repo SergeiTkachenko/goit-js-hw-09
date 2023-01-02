838e150cf96afd8e54a2c6bb2e42b9ca75fbb201
SergeiTkachenko <fox.sergei92@gmail.com>
2023-01-02 10:48:17 +0200

    add 1 hw

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const randomHexColor = getRandomHexColor();
+    bodyEl.setAttribute('style', `background-color: ${randomHexColor}`);
+    btnStart.disabled = true;
+  }, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+});
