181955a41d37c0e22edbda4c408e1dd560d23403
SergeiTkachenko <fox.sergei92@gmail.com>
2023-01-04 08:50:43 +0200

    add hw 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,77 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const inputEl = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const daysEl = document.querySelector('span[data-days]');
+const hoursEl = document.querySelector('span[data-hours]');
+const minutesEl = document.querySelector('span[data-minutes]');
+const secondsEl = document.querySelector('span[data-seconds]');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const calendar = flatpickr(inputEl, options);
+
+calendar.config.onChange.push(onDataChange);
+
+function onDataChange() {
+  const selectedCalendarDates = calendar.selectedDates[0];
+  const pastOrFuture = selectedCalendarDates - new Date();
+
+  if (pastOrFuture <= 0) {
+    startBtn.disabled = true;
+    window.alert('Please choose a date in the future');
+  } else {
+    startBtn.disabled = false;
+  }
+}
+
+startBtn.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    const selectedCalendarDates = calendar.selectedDates[0];
+    let pastOrFuture = selectedCalendarDates - new Date();
+
+    const getDatesFromMs = convertMs(pastOrFuture);
+
+    if (pastOrFuture < 0) {
+      clearInterval(intervalId);
+      return;
+    }
+    daysEl.textContent = getDatesFromMs.days.toString().padStart(2, '0');
+    hoursEl.textContent = getDatesFromMs.hours.toString().padStart(2, '0');
+    minutesEl.textContent = getDatesFromMs.minutes.toString().padStart(2, '0');
+    secondsEl.textContent = getDatesFromMs.seconds.toString().padStart(2, '0');
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(intervalId);
+});
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
