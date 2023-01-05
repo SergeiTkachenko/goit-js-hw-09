190d8a931d6dbed2942bd89296d994c05914ed2b
SergeiTkachenko <fox.sergei92@gmail.com>
2023-01-05 19:07:32 +0200

    add hw3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onClickSubmit);
+
+function onClickSubmit(e) {
+  e.preventDefault();
+
+  const formElements = e.target.elements;
+  const formAmount = Number(formElements.amount.value);
+  let formDelay = Number(formElements.delay.value);
+  const formStep = Number(formElements.step.value);
+
+  for (let i = 1; i <= formAmount; i += 1) {
+    createPromise(i, formDelay);
+    formDelay += formStep;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  new Promise((Fulfill, Reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        Fulfill(console.log(`✅ Fulfilled promise ${position} in ${delay}ms`));
+      } else {
+        Reject(console.log(`❌ Rejected promise ${position} in ${delay}ms`));
+      }
+    }, delay);
+  });
 }
