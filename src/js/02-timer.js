966c8d0026af333e089bfa9ae424fa00b530a948
SergeiTkachenko <fox.sergei92@gmail.com>
2023-01-05 08:36:51 +0200

    fix style

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.css';
+import 'src/css/common.css';
 
 const inputEl = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('button[data-start]');
