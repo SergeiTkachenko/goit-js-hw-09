b178c242d5c99dd6419c9c2f49f2301ba52e83b1
SergeiTkachenko <92872700+SergeiTkachenko@users.noreply.github.com>
2023-01-06 05:05:53 +0000

    Deploying to gh-pages from @ SergeiTkachenko/goit-js-hw-09@60293fe7318fb3bc440af8986b9c076adbf444b2 🚀

--- 03-promises.e93f9d9c.js
+++
@@ -1,2 +0,0 @@
-!function(){function e(e,n){var o=Math.random()>.3;new Promise((function(t,c){setTimeout((function(){o?t(console.log("✅ Fulfilled promise ".concat(e," in ").concat(n,"ms"))):c(console.log("❌ Rejected promise ".concat(e," in ").concat(n,"ms")))}),n)}))}document.querySelector(".form").addEventListener("submit",(function(n){n.preventDefault();for(var o=n.target.elements,t=Number(o.amount.value),c=Number(o.delay.value),u=Number(o.step.value),a=1;a<=t;a+=1)e(a,c),c+=u}))}();
-//# sourceMappingURL=03-promises.e93f9d9c.js.map
