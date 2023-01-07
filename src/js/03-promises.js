6849a5c0ea65a0649df5972c9b30b524799e97fc
SergeiTkachenko <fox.sergei92@gmail.com>
2023-01-07 10:17:44 +0200

    fix hw 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -11,26 +11,29 @@ function onClickSubmit(e) {
   let formDelay = Number(formElements.delay.value);
   const formStep = Number(formElements.step.value);
 
-  for (let i = 0; i < formAmount; i += 1) {
-    let parameters = {};
-    parameters.position = i + 1;
-    parameters.delay = formDelay + formStep * i;
-    createPromise(parameters);
+  for (let i = 1; i <= formAmount; i += 1) {
+    formDelay += formStep;
+    createPromise(i, formDelay);
   }
 }
 
-function createPromise({ position, delay }) {
+function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
 
-  new Promise((Fulfill, Reject) => {
+  const promise = new Promise((fulfill, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
-        Fulfill(
-          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
-        );
+        fulfill(`✅ Fulfilled promise ${position} in ${delay}ms`);
       } else {
-        Reject(Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
       }
     }, delay);
   });
+  promise
+    .then(value => {
+      Notify.success(value);
+    })
+    .catch(err => {
+      Notify.failure(err);
+    });
 }
